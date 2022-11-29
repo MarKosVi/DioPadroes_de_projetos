1579e93ffd7c0c62b7ae27c5cc638f9ddc40dfc2
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-11-28 22:16:12 -0300

    Singleton

--- src/Singleton/main.java
+++ src/Singleton/main.java
@@ -4,8 +4,14 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Test");
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
 		
+		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		
+		SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
 	}
 
 }
